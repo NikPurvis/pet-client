5c1bfc478d2fc1e05af5b7a0d3097c236633b4f2
NikPurvis <nikpurvis@gmail.com>
2022-03-31 08:37:22 -0600

    Created new pet function call to API

--- src/api/pets.js
+++ src/api/pets.js
@@ -5,3 +5,30 @@ import axios from "axios"
 export const getAllPets = () => {
     return axios(`${apiUrl}/pets`)
 }
+
+
+// SHOW function
+// Just gets the petId and passes it
+// "export" at the beginning of the function makes it easy to destructure when you need it.
+export const getOnePet = (petId) => {
+    return axios(`${apiUrl}/pets/${petId}`)
+}
+
+// POST -> create function
+// Arguments are passing the things we need to the API. newPet is the fields filled in for the new pet.
+// We'll send an anonymous function
+export const createPet = (user, newPet) => {
+    return axios ({
+        url: `${apiUrl}/pets`,
+        method: "POST",
+        headers: {
+            Authorization: `Token token = ${user.token}`
+        },
+        data: newPet
+    })
+
+}
+
+// PATCH -> update function
+
+// DELETE -> remove function
